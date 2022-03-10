e59b4f16590d2910e1a81a72d745f31001ec7cfb
Erdem TOPAL <61634463+erdemtopal@users.noreply.github.com>
2022-03-10 19:22:03 +0300

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,3 @@
 class Message < ApplicationRecord
 	validates :body, presence: true
-	broadcasts_to ->(message) { :inbox_list }, inserts_by: :prepend, target: 'messages'
-
-	after_commit :send_html_counter, on: [ :create, :destroy ]
-	  def send_html_counter
-	    broadcast_update_to('inbox_list', target: 'sayi_revize', html: "#{Message.count}")
-	    # broadcast_update_to('inbox_list', target: 'inbox_count', html: Inbox.count)
-	  end
-
-
 end
